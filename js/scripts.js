4c97c0b370b70def60b713104f0caca9f5b38c18
Student <example@example.com>
2019-10-29 14:12:43 -0700

    add a kind of working, mostly not factorial file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
+var counter = function(number) {
+  for(var i = 0; i < number.length; i++){
+    var total = i *= number[i];
+  }
+  return total;
+}
+
 $(document).ready(function(){
   $('form#factorialForm').submit(function(){
     event.preventDefault();
     var factorialInput = parseInt($('input#factorialInput').val());
+    var finalFactorialArray = []
 
     for(var i = factorialInput; i > 0; i--){
-      console.log(i);
-      var finalFactorial = [];
-      finalFactorial = (factorialInput * i);
-      console.log(finalFactorial);
-
+      var test = i * [i]-1
+      console.log("Test: ", test);
+      console.log("i: ", i);
+      finalFactorialArray.push(i);
     }
-
+    $('#results').append(counter(finalFactorialArray));
+    console.log(finalFactorialArray);
   });
 });
